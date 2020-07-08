3219226d17ec263ab1f6029086232e760e0b7e6c
Fairmount Software <fairmountsoftware@gmail.com>
2020-07-08 19:52:24 -0400

    Initial rev.

---
+++ CSharpProjects/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿/*************
+ * ConsoleApp
+ * by Jacob L. Haskins
+ * July 8, 2020
+ * Copyright (c) 2020.  All Rights Reserved.
+**************/
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n\nPress Enter to continue.");
+            Console.ReadLine();
+        }
+    }
+}
